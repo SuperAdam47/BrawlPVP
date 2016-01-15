56b7d38e3b3c46e40aac1203d433ddc0be0761be
TutoGamerWalid <wiligangster@users.noreply.github.com>
2016-01-16 00:00:09 +0100

    Update Main.php

--- src/BrawlPVP/Main.php
+++ src/BrawlPVP/Main.php
@@ -1,2 +1,31 @@
 <?php
-namespace: BrawlPVP
+//le plugin est créer part TutoGamerWalid & MrReskill copyright 2015 - 2016 ©
+
+namespace BrawlPVP;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\utils\TextFormat;
+use pocketmine\entity\Entity;
+use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\tile\Sign;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\item\Item;
+use pocketmine\Server;
+use pocketmine\Player;
+
+class Main extends PluginBase Implements Listener {
+	//scheduleDelayedTask
+	public $cfg;
+	public $players;
