916b2a62e3b3d4b39834805a6d40e3b65b8df589
TutoGamerWalid <wiligangster@users.noreply.github.com>
2016-01-16 00:52:55 +0100

    Update Main.php

--- src/BrawlPVP/Main.php
+++ src/BrawlPVP/Main.php
@@ -29,6 +29,7 @@ class Main extends PluginBase Implements Listener {
 	//scheduleDelayedTask
 	public $cfg;
 	public $players;
+        public $gameStarted = [];
 	
 		public function onEnable() {
 		$this->saveDefaultConfig();
